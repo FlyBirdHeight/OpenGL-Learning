61082ecf970287ee862dabc5945955ce781059cc
adsionli <adsionli@adsionlideMacBook-Pro.local>
2020-07-30 20:20:54 +0800

    ljq

---
+++ opengl/triangle.cpp
@@ -0,0 +1,9 @@
+//
+//  triangle.cpp
+//  opengl
+//
+//  Created by adsionli on 2020/7/30.
+//  Copyright © 2020 adsionli. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ opengl/window_show.cpp
@@ -0,0 +1,9 @@
+//
+//  window_show.cpp
+//  opengl
+//
+//  Created by adsionli on 2020/7/30.
+//  Copyright © 2020 adsionli. All rights reserved.
+//
+
+#include <stdio.h>
